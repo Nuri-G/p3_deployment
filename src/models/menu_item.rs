c473da61d7f321779094c80ceb565d9360d0d5fd
Evan De la Garza <eadlg2@tamu.edu>
2022-11-12 15:10:07 -0600

    Added menu put endpoint

--- src/models/menu_item.rs
+++ src/models/menu_item.rs
@@ -1,11 +1,11 @@
 use serde::{Serialize, Deserialize};
-use actix_web::{get, web, Result, Responder, post, HttpResponse};
+use actix_web::{get, web, Result, Responder, post, HttpResponse, put};
 
 use crate::models::helpers::make_connection_pool;
 
 #[derive(Serialize, Deserialize, sqlx::FromRow)]
 pub struct MenuItem {
-    pub id: i32,
+    pub id: Option<i32>,
     pub name: String,
     pub category: String,
     pub ingredients_inventory_id: Vec<i32>,
@@ -23,8 +23,20 @@ pub async fn get_menu() -> Result<impl Responder> {
 #[post("/api/menu")]
 pub async fn post_menu(data: web::Json<MenuItem>) -> HttpResponse {
     let pool = make_connection_pool().await;
-    match sqlx::query!("INSERT INTO menu_items (id, name, ingredients_inventory_id, category, price, description) VALUES ($1, $2, $3, $4, $5, $6)",
-        data.id, data.name, &data.ingredients_inventory_id, data.category, data.price, data.description)
+    match sqlx::query!("INSERT INTO menu_items (name, ingredients_inventory_id, category, price, description) VALUES ($1, $2, $3, $4, $5)",
+        data.name, &data.ingredients_inventory_id, data.category, data.price, data.description)
+        .execute(&pool)
+        .await {
+            Ok(_) => HttpResponse::Ok().finish(),
+            Err(_) => HttpResponse::BadRequest().finish(),
+        }
+}
+
+#[put("/api/menu")]
+pub async fn put_menu(data: web::Json<MenuItem>) -> HttpResponse {
+    let pool = make_connection_pool().await;
+    match sqlx::query!("UPDATE menu_items SET name = $1, ingredients_inventory_id = $2, category = $3, price = $4, description = $5 WHERE id = $6",
+        data.name, &data.ingredients_inventory_id, data.category, data.price, data.description, data.id)
         .execute(&pool)
         .await {
             Ok(_) => HttpResponse::Ok().finish(),
